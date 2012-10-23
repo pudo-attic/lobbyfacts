6c258d0697649a48a643fa65d28728d8afbf1345
Friedrich Lindenberg <friedrich@pudo.org>
2012-10-23 14:31:22 +0200

    Rename module externally

--- lobbyfacts/default_settings.py
+++ lobbyfacts/default_settings.py
@@ -2,15 +2,15 @@
 DEBUG = True
 CACHE = False
 SECRET_KEY = 'no'
-SITE_TITLE = 'openinterests.eu'
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/openinterests'
+SITE_TITLE = 'lobbyfacts.eu'
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/lobbyfacts'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///local.db'
 
 NOMENKLATURA_URL = 'http://nomenklatura.okfnlabs.org'
 NOMENKLATURA_APIKEY = None
 
 ETL_URL = 'sqlite:///etl.db'
-ETL_URL = 'postgresql://localhost/openinterests_etl'
+ETL_URL = 'postgresql://localhost/lobbyfacts_etl'
 
 ETL_PYBOSSA_HOST = 'http://pybossa.com'
 ETL_PYBOSSA_KEY = None # fill in from http://pybossa.com/account/profile
