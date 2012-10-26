f506061280d04e781f39e07a73720516875451a7
Friedrich Lindenberg <friedrich@pudo.org>
2012-10-26 12:41:48 +0200

    Add CSV export to reports.

--- lobbyfacts/views/reports.py
+++ lobbyfacts/views/reports.py
@@ -3,10 +3,20 @@ import inspect
 
 from lobbyfacts.exc import NotFound
 from lobbyfacts.model import REPORTS
+from lobbyfacts.util import response_format, stream_csv
 from lobbyfacts.util import jsonify, validate_cache
 from lobbyfacts.views.util import get_limit, get_offset, paged_url
 
 reports = Blueprint('reports', 'reports')
+
+def get_results(q):
+    results = q
+    if len(q.column_descriptions) > 1:
+        results = []
+        headers = [cd['name'] for cd in q.column_descriptions]
+        for result in q:
+            results.append(dict(zip(headers, result)))
+    return results
 
 @reports.route('/reports')
 def report_index():
@@ -37,18 +47,17 @@ def report(name, format=None):
         q = func(**args)
     except Exception, exc:
         return jsonify({'error': unicode(exc)}, status=400)
+    
+    format = response_format(request)
+    if format == 'csv':
+        return stream_csv(get_results(q), filename="%s.csv" % name)
+
 
     count = q.count()
     limit = get_limit()
     q = q.limit(limit)
     offset = get_offset()
     q = q.offset(offset)
-    results = q
-    if len(q.column_descriptions) > 1:
-        results = []
-        headers = [cd['name'] for cd in q.column_descriptions]
-        for result in q:
-            results.append(dict(zip(headers, result)))
 
     return jsonify({
         'count': count,
@@ -57,5 +66,5 @@ def report(name, format=None):
         'limit': limit,
         'offset': offset,
         'query': str(q),
-        'results': results
+        'results': get_results(q)
         }, shallow=True)
