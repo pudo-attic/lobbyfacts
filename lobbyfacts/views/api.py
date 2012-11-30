90042fea0a315b8fd0a87a896d58891a5e1deda9
Friedrich Lindenberg <friedrich@pudo.org>
2012-11-30 11:41:47 +0100

    Support facets in browser

--- lobbyfacts/views/api.py
+++ lobbyfacts/views/api.py
@@ -2,6 +2,7 @@ from itertools import groupby
 
 from flask import Blueprint, request, redirect, url_for
 
+from lobbyfacts.core import db
 from lobbyfacts.model.entity import Entity
 from lobbyfacts.exc import NotFound, BadRequest
 from lobbyfacts.util import jsonify, validate_cache
@@ -11,23 +12,47 @@ from lobbyfacts.views.util import get_limit, get_offset, paged_url
 
 def make_entity_api(cls):
     name = cls.__tablename__
+    mapper = cls.__mapper__
     api = Blueprint(name, name)
 
-    def filter_query():
-        query = cls.all()
+    def filter_query(query):
+        query.has_fts = False
+        filters = []
         try:
             filter_ = [f.split(':', 1) for f in request.args.getlist('filter')]
             for key, values in groupby(filter_, lambda a: a[0]):
+                key = key if key in mapper.c else key + '_id'
                 attr = getattr(cls, key)
                 clause = db.or_(*[attr == v[1] for v in values])
                 query = query.filter(clause)
             fts_query = request.args.get('q', '').strip()
             if len(fts_query) and hasattr(cls, 'entity'):
+                query.has_fts = True
                 query = query.join(Entity)
                 query = query.filter('entity.full_text @@ plainto_tsquery(:ftsq)')
-                query = query.order_by('ts_rank_cd(entity.full_text, plainto_tsquery(:ftsq)) DESC')
                 query = query.params(ftsq=fts_query)
             return query
+        except (ValueError, AttributeError, IndexError) as e:
+            raise BadRequest(e)
+
+    def make_facets():
+        facets = {}
+        try:
+            for facet in request.args.getlist('facet'):
+                count = db.func.count(cls.id)
+                attr = getattr(cls, facet)
+                if facet in mapper.c:
+                    query = db.session.query(attr, count)
+                    query = query.group_by(attr)
+                else:
+                    pcls = mapper.get_property(facet).mapper.class_
+                    query = db.session.query(pcls, count)
+                    query = query.join(attr)
+                    query = query.group_by(pcls.id)
+                query = filter_query(query)
+                query = query.order_by(count.desc())
+                facets[facet] = query
+            return facets
         except (ValueError, AttributeError, IndexError) as e:
             raise BadRequest(e)
 
@@ -35,7 +60,7 @@ def make_entity_api(cls):
     @api.route('/%s' % name)
     def index(format=None):
         request.cache_key['args'] = request.args.items()
-        q = filter_query()
+        q = filter_query(cls.all())
 
         format = response_format(request)
         if format == 'csv':
@@ -48,6 +73,8 @@ def make_entity_api(cls):
             return stream_csv(generate(), filename="%s.csv" % name)
 
         count = q.count()
+        if q.has_fts:
+            q = q.order_by('ts_rank_cd(entity.full_text, plainto_tsquery(:ftsq)) DESC')
         limit = get_limit()
         q = q.limit(limit)
         offset = get_offset()
@@ -58,7 +85,8 @@ def make_entity_api(cls):
             'previous': paged_url('.index', limit, offset-limit) if offset > 0 else False,
             'limit': limit,
             'offset': offset,
-            'results': q
+            'results': q,
+            'facets': make_facets()
             }, shallow=True)
 
     @api.route('/%s/<id>' % name)
